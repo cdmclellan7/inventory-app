e8875bfd2fe9ded4830edca3fe5c26193aa4d0c5
cdmclellan7 <cdmclellan7@gmail.com>
2019-11-15 18:24:50 -0500

    Changed models' url virtual

--- models/item.js
+++ models/item.js
@@ -15,7 +15,7 @@ const ItemSchema = new Schema(
 ItemSchema
 .virtual('url')
 .get(function() {
-    return '/items' + this._id;
+    return '/inventory/item/' + this._id;
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
